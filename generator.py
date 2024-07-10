c992dbbc07e772dee707b4e62a98d6de809bed40
abhay <abhay.i0am@gmail.com>
2024-07-10 09:55:47 +0530

    made py password generator

---
+++ generator.py
@@ -0,0 +1,11 @@
+import random
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "0123456789"
+symbol = "!@#$%^&*()_-=[]}{'.,?><"
+
+all = lower + upper + upper + symbol + number + lower + number + symbol
+length = 12
+password = "".join(random.sample(all,length))
+print("The Password generated for you is :- ",password)
