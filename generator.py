08a9d279f2625066caebb05dd79bf5590c7923b1
abhay <abhay.i0am@gmail.com>
2024-07-10 09:59:34 +0530

    fixed a problem
    it was stopping instantly upon password generation, fixed it..

--- generator.py
+++ generator.py
@@ -9,3 +9,5 @@ all = lower + upper + upper + symbol + number + lower + number + symbol
 length = 12
 password = "".join(random.sample(all,length))
 print("The Password generated for you is :- ",password)
+
+input("Press Enter to exit...")
